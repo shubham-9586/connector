1313a13fe9695252942f079f3a62baf7e18070b0
shubham kesharwani <shubham.kesharwani@bikayi.com>
2022-06-10 21:00:22 +0530

    little change

--- js/client.js
+++ js/client.js
@@ -1,43 +1,40 @@
-const socket=io('https://connecter-123.herokuapp.com');
-const form=document.getElementById('send-container');
-const messageInput=document.getElementById('messageInp');
-const messagecontainer=document.querySelector(".container");
+const socket = io("https://connecter-123.herokuapp.com");
+const form = document.getElementById("send-container");
+const messageInput = document.getElementById("messageInp");
+const messagecontainer = document.querySelector(".container");
 
-var audio= new Audio('sound.mp3');
+var audio = new Audio("sound.mp3");
 
-const append = (message ,position)=>{
-    const messageElement= document.createElement('div');
-    messageElement.innerHTML=message;
-    messageElement.classList.add('message');
-    messageElement.classList.add(position);
-     messagecontainer.append(messageElement);
-     if(position=='left')
-     {
-     audio.play();
-     }
-}
+const append = (message, position) => {
+  const messageElement = document.createElement("div");
+  messageElement.innerHTML = message;
+  messageElement.classList.add("message");
+  messageElement.classList.add(position);
+  messagecontainer.append(messageElement);
+  if (position == "left") {
+    audio.play();
+  }
+};
 
-form.addEventListener('submit', e=>{
-e.preventDefault();
-const message=messageInput.value;
-append(`you:${message}`,'right');
-socket.emit('send',message);
-messageInput.value="";
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = messageInput.value;
+  append(`you:${message}`, "right");
+  socket.emit("send", message);
+  messageInput.value = "HI";
 });
 
-const name=prompt("Enter your name");
+const name = prompt("Enter your name");
 
-
-    socket.emit('new-user-joined' , name);
-socket.on('user-joined' ,name=>{
-    append(`${name} joined the chat` ,'right');
+socket.emit("new-user-joined", name);
+socket.on("user-joined", (name) => {
+  append(`${name} joined the chat`, "right");
 });
 
-socket.on('receive' , data=>{
-    append(`${data.name} : ${data.message}` ,'left')
-})
+socket.on("receive", (data) => {
+  append(`${data.name} : ${data.message}`, "left");
+});
 
-socket.on('left', name=>{
-   
-    append(`${name} left the chat`,'left');
-})
+socket.on("left", (name) => {
+  append(`${name} left the chat`, "left");
+});
